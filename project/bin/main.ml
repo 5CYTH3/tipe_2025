7cfe630e38c279a10a3b5f26edc938202eb43e71
5CYTH3 <ScytheD@outlook.fr>
2025-05-20 23:12:01 +0200

    Added new test

--- project/bin/main.ml
+++ project/bin/main.ml
@@ -1,11 +1,8 @@
-(*
 let ctx = Types.TypeMap.of_list [
-    "+", Types.Abs (Types.Int, Types.Abs (Types.Int, Types.Int)); 
+    "add", Types.Abs (Types.Int, Types.Abs (Types.Int, Types.Int)); 
     "y", Types.Int
     (* App is left-assoc but TAbs is right-assoc *)
 ]
-*)
-
 
 let from_file f =
     let ic = open_in f in
@@ -13,7 +10,7 @@ let from_file f =
     let lexer = Lexer.collect_tokens lexbuf [] in
     (* print_string @@ Parser.show_program lexer; *)
     let start = Sys.time () in
-    let (t, _) = Parser.parse lexer |> Types.infer_types Types.TypeMap.empty in
+    let (t, _) = Parser.parse lexer |> Types.infer_types ctx in
     let stop = Sys.time () in
     let elapsed = stop -. start in
     print_float elapsed;
@@ -37,6 +34,6 @@ let show_program (e, t, ctx) =
 *)
 
 let () =
-    from_file "test/main.lc" |> ignore
+    from_file "test/main2.lc" |> ignore
     (* |> show_program *)
 
