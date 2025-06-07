e5502f7d2346cf4af80f3e25486233455d04df9f
5CYTH3 <ScytheD@outlook.fr>
2025-06-07 15:32:45 +0200

    Deleted everything except the project

--- project/bin/main.ml
+++ project/bin/main.ml
@@ -1,31 +1,5 @@
+open Cmdliner
 
-let ctx = Types.TypeMap.of_list [
-    "add", Types.Abs (Types.Int, Types.Abs (Types.Int, Types.Int)); 
-    "y", Types.Int
-    (* App is left-assoc but TAbs is right-assoc *)
-]
-
-
-let from_file f =
-    let ic = open_in f in
-    let lexbuf = really_input_string ic (in_channel_length ic) |> Lexing.from_string in
-    let lexer = Lexer.collect_tokens lexbuf [] in
-    (* print_string @@ Parser.show_program lexer; *)
-    let start = Sys.time () in
-    let result = Parser.parse lexer ctx in
-    let stop = Sys.time () in
-    let elapsed = stop -. start in
-    print_float elapsed;
-    result
-;;
-
-(*
-let from_str s = 
-    let lexbuf = s |> Lexing.from_string in
-    let lexer = Lexer.collect_tokens lexbuf [] in
-    (* print_string @@ Parser.show_program lexer; *)
-    Parser.parse lexer ctx
-;;
 
 let show_program (e, t, ctx) = 
     Printf.printf "\nCTX: %s\nTYPE: %s\nEXPR:\n%s\n\n"
@@ -33,9 +7,77 @@ let show_program (e, t, ctx) =
         (Types.show t)
         (Parser.show_expr e)
 ;;
-*)
 
-let () =
-    from_file "test/main2.lc" |> ignore
-    (* |> show_program *)
+let from_file f b_time =
+    let ic = open_in f in
+    let lexbuf = really_input_string ic (in_channel_length ic) |> Lexing.from_string in
+    let lexer = Lexer.collect_tokens lexbuf [] in
+    (* print_string @@ Parser.show_program lexer; *)
+    let start = Sys.time () in
+    let res = Parser.parse lexer Types.TypeMap.empty in
+    let stop = Sys.time () in
+    let elapsed = stop -. start in
+    if b_time then ( 
+        print_float elapsed
+    ) else show_program res
+;;
+
+let from_str s b_time = 
+    let lexbuf = s |> Lexing.from_string in
+    let lexer = Lexer.collect_tokens lexbuf [] in
+    let start = Sys.time () in
+    let res = Parser.parse lexer Types.TypeMap.empty in
+    let stop = Sys.time () in
+    let elapsed = stop -. start in
+    if b_time then ( 
+        print_float elapsed
+    ) else show_program res
+
+;;
+
+let main t o string_opt file_opt =
+    match (t, o) with
+    | true, true -> 
+        Printf.eprintf "Error: only one of -t or -o can be specified\n";
+        `Error (false, "Conflicting flags")
+    | _ -> begin
+        match string_opt, file_opt with
+        | None, None ->
+            Printf.eprintf "Error: Provide either a string or --file option\n";
+            `Error (false, "Missing argument")
+        | Some s, None ->
+            from_str s t;
+            `Ok ()
+        | None, Some f ->
+            from_file f t;
+            `Ok ()
+        | Some _, Some _ ->
+            Printf.eprintf "Error: Provide either a string or --file option, not both\n";
+            `Error (false, "Conflicting arguments")
+    end
+
+let flag_t =
+  let doc = "Option t" in
+  Arg.(value & flag & info ["t"] ~doc)
+
+let flag_o =
+  let doc = "Option o" in
+  Arg.(value & flag & info ["o"] ~doc)
+
+let string_arg =
+  let doc = "Input string" in
+  Arg.(value & pos 0 (some string) None & info [] ~docv:"STRING" ~doc)
+
+let file_arg =
+  let doc = "Input file" in
+  Arg.(value & opt (some string) None & info ["file"] ~docv:"FILE" ~doc)
+
+let cmd =
+  let info = Cmd.info "build" ~doc:"Example CLI with flags and input" in
+  Cmd.v info
+    Term.(ret (const main $ flag_t $ flag_o $ string_arg $ file_arg))
+
+let () = 
+    exit @@ Cmd.eval cmd
+
 
